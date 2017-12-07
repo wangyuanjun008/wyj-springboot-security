55998580a82033bf145fd319b9fd170f1d82bbdf
wangyuanjun008 <1098345452@qq.com>
2017-12-07 22:42:36 +0800

    1.所有方法删除@ResponseBody注解，类上加@RestController注解。2.修改获取下拉框数据源方法

--- src/main/java/com/wyj/controller/data/DataDictController.java
+++ src/main/java/com/wyj/controller/data/DataDictController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
@@ -35,7 +36,7 @@ import com.wyj.service.data.DataGroupService;
  * @author：WangYuanJun
  * @date：2017年11月22日 下午8:25:35
  */
-@Controller
+@RestController
 @RequestMapping(value = "/dataDict")
 public class DataDictController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -49,12 +50,6 @@ public class DataDictController {
     @Resource
     private RedisTemplate<String, DataDict> redisTemplate;
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    private String index() {
-        return "/data/dataDict";
-    }
-
-    @ResponseBody
     @RequestMapping(value = "/list", method = RequestMethod.POST)
     public String query(@RequestParam(value = "offset", required = true, defaultValue = "1") Integer page, @RequestParam(value = "limit", required = false, defaultValue = "10") Integer pageSize, Long dataGroupId) {
         if (dataGroupId == null) {
@@ -70,7 +65,6 @@ public class DataDictController {
         return JSON.toJSONString(pageInfo.getList());
     }
 
-    @ResponseBody
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public Retval save(DataDict dataDict) {
         Retval retval = Retval.newInstance();
@@ -88,12 +82,11 @@ public class DataDictController {
         return retval;
     }
 
-    @ResponseBody
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Retval edit(@PathVariable String id) {
         Retval retval = Retval.newInstance();
         if (redisTemplate.hasKey("dataDict" + id)) {
-            DataDict dataDict = redisTemplate.opsForValue().get("dataGroup" + id);
+            DataDict dataDict = redisTemplate.opsForValue().get("dataDict" + id);
             retval.put("obj", dataDict);
         } else {
             DataDict dataDict = dataDictService.getObjectById(Long.valueOf(id));
@@ -102,7 +95,6 @@ public class DataDictController {
         return retval;
     }
 
-    @ResponseBody
     @RequestMapping(value = "/remove", method = RequestMethod.POST)
     public Retval remove(@RequestParam Long[] ids) {
         Retval retval = Retval.newInstance();
@@ -124,7 +116,6 @@ public class DataDictController {
      * 
      * @return
      */
-    @ResponseBody
     @RequestMapping(value = "/renderTree", method = RequestMethod.GET)
     public List<Map<String, Object>> renderTree() {
         List<Map<String, Object>> returnList = new ArrayList<Map<String, Object>>();
@@ -145,11 +136,18 @@ public class DataDictController {
      * @param groupCode
      * @return
      */
-    @ResponseBody
     @RequestMapping(value = "/getData", method = RequestMethod.GET)
     public String getDictCodeNameByGroupCode(@RequestParam String groupCode) {
-
         List<Map<Long, String>> map = dataDictService.getDictCodeNameByGroupCode(groupCode);
+        List<Map<String, Object>> map1 = new ArrayList<Map<String, Object>>();
+        for (Map<Long, String> map2 : map) {
+            String id = map2.get("id");
+            String text = map2.get("text");
+            Map<String, Object> map3 = new HashMap<String, Object>();
+            map3.put("id", Long.valueOf(id.toString()));
+            map3.put("text", text);
+            map1.add(map3);
+        }
         return JSON.toJSONString(map);
     }
 }
